db8711e00e212d8429e125d558df7bf1943bc005
Superior-Snacks <joningiwork@gmail.com>
2025-04-18 18:57:51 +0000

    s

--- securevault/vault/models.py
+++ securevault/vault/models.py
@@ -5,4 +5,4 @@ class Credential(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='credentials')
     website_url = models.URLField(max_length=255)
     login = models.CharField(max_length=255)
-    encrypted_password = models.CharField(max_length=255)
+    encrypted_password = models.TextField(max_length=255)
