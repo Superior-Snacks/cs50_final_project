03806cebaa40e2bba6933bb65491850cfc98ed6c
Superior-Snacks <joningiwork@gmail.com>
2025-04-18 18:56:51 +0000

    s

--- securevault/vault/models.py
+++ securevault/vault/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Cred(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cred')
+class Credential(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='credentials')
+    website_url = models.URLField(max_length=255)
+    login
