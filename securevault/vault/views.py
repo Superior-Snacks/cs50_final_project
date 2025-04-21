2cdd85068ab873f10725f3988bef62dff2951a71
Superior-Snacks <joningiwork@gmail.com>
2025-04-21 18:06:21 +0000

    s

--- securevault/vault/views.py
+++ securevault/vault/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def login():
     ...
 
-def register():
-    ...
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            return render(request, "vault.register.html")
 
 def logout():
     ...
