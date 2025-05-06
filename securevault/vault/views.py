312ff31e9417d158eba03906c9667c58ad528786
Superior-Snacks <joningiwork@gmail.com>
2025-05-06 19:02:58 +0000

    s

--- securevault/vault/views.py
+++ securevault/vault/views.py
@@ -23,6 +23,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            return render(request, "vault/vault.html")
         else:
             return render(request, "vault/register.html")
 
