62f01670bd189192b1fc3e7915cd9081b7342874
Superior-Snacks <joningiwork@gmail.com>
2025-05-06 17:26:09 +0000

    s

--- securevault/vault/urls.py
+++ securevault/vault/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("vault", views.vault, name="vault"),
-    path("login", views.login, name="login"),
-    path("register", views.register, name="register"),
-    path("logout", views.logout, name="logout")
+
+    path("vault/", views.vault, name="vault"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout, name="logout")
 
 
 ]
