135a5e0632d47f1180c67bf2aef3ab655ad59087
Superior-Snacks <joningiwork@gmail.com>
2025-05-06 17:05:46 +0000

    s

--- securevault/vault/views.py
+++ securevault/vault/views.py
@@ -31,4 +31,8 @@ def logout(request):
 
 
 def vault(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
     return render(request, "vault/vault.html")
