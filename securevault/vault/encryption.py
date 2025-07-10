6008c90ea377785be1073b117231046e06706dab
Superior-Snacks <joningiwork@gmail.com>
2025-07-10 21:43:01 +0000

    restart

--- securevault/vault/encryption.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-
-def make_key():
-    return Fernet.generate_key()
-
-print(make_key())
--- securevault/vault/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Credential
-
-class CredentialForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = Credential
-        fields = ['website', 'login', 'password']
